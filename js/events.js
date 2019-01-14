06aa2f475ccd1e893cd7cdb0f98c461b13047de8
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-14 17:46:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert('G was typed');
+    }
+  });
+}
 $(document).ready(function(){
 
 // call functions here
 
+getIt();
+pressIt();
+frameIt();
+submitIt();
 });
